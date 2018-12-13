20bad25db8f73382a0a0c7bea67f1cf6d7caa74d
licia_tia <licia@s-cry.com>
2018-12-13 14:47:07 +0800

    durfex_tf_idf_matrix_done

--- durfex/gen_matrix.py
+++ durfex/gen_matrix.py
@@ -1,11 +1,27 @@
 import json
+import math
 
 
 class Report:
     grams = []
 
 
-f = open('../stack_data.json', 'r')
+def tf_idf(_gram, _report, _reports):
+    n = 0
+    for something in _report.grams:
+        if _gram == something:
+            n = n + 1
+    tf = n / len(_report.grams)
+
+    m = 1
+    for somereport in _reports:
+        if somereport.grams.__contains__(_gram):
+            m = m + 1
+
+    return tf * math.log(len(_reports)/m)
+
+
+f = open('../dataset/stack_data.json', 'r')
 data = json.load(f)
 n_grams = set()
 reports = []
@@ -14,6 +30,7 @@ i = 0
 
 for report in data:
     tmp = Report()
+    tmp.grams = []
     # 1-gram
     for stack in report['stack_arr']:
         n_grams.add(stack['symbol'])
@@ -34,7 +51,7 @@ for report in data:
                          + report['stack_arr'][i + 2]["symbol"])
 
     reports.append(tmp)
-    del tmp
+    # print(tmp.grams)
 
 n_grams_dict = {}
 i = 0
@@ -44,13 +61,11 @@ for gram in n_grams:
 
 matrix = []
 
-
 # calculating tf-idf
 for report in reports:
-    tmp_mat = [0]*len(n_grams)
+    tmp_mat = [0] * len(n_grams)
     for gram in report.grams:
-        tmp_mat[n_grams_dict.get(gram)] = 1
+        tmp_mat[n_grams_dict.get(gram)] = tf_idf(gram, report, reports)
     matrix.append(tmp_mat)
-    del tmp_mat
 
 print(matrix)
