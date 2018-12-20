165eb5b8718bd2b240c4d8122c66b7430f184aff
neo <nexoimy@gmail.com>
2018-12-20 22:52:29 +0800

    update alg

--- Wednesday/algorithm.py
+++ Wednesday/algorithm.py
@@ -52,6 +52,12 @@ class Algorithm:
 
     def start(self, report_set1, report_set2):
         try:
+            if len(report_set1["stack_arr"]) == 0 \
+                or len(report_set1["stack_arr"]) == 0:
+                return False
+            # #demo
+            # if self.calculate(report_set1["stack_arr"][0], report_set2["stack_arr"][0]):
+            #     return True
             for stack_0 in report_set1["stack_arr"]:
                 for stack_1 in report_set2["stack_arr"]:
                     if self.calculate(stack_0, stack_1):
@@ -71,13 +77,18 @@ class ResultUtils:
             list_.append('TRUE' if v else 'FALSE')
         return list_
 
+    # fliter: 表示是否去除 real: False, pred: True 的数据，这类数据很有可能是预测成功但未人工标记的数据
     # [{'450439 275972': [False, False]}] => [False] [False]
     # [{'450439 275972': [False, False]}, {'450439 16749': [False, False]}] => [False, False] [False, False]
-    def format(self, src_list):
+    def format(self, src_list, fliter=False):
+        # print('Fliter: ', fliter)
         list1 = [] # real
         list2 = [] # pred
         for dict_ in src_list:
             for v in dict_.values():
+                if fliter == True:
+                    if v[0] == False and v[1] == True:
+                        continue
                 list1.append(v[0])
                 list2.append(v[1])
         return {'real_list': list1, 'pred_list': list2}
@@ -95,12 +106,16 @@ class ResultUtils:
         return {'real_list': list1, 'pred_list': list2}
 
     # （辅助函数）打印两个数组不同处
+    # fliter: 表示是否去除 real: False, pred: True 的数据，这类数据很有可能是预测成功但未人工标记的数据
     # [{'450439 275972': [False, False]}]
-    def show_diff(self, dicts):
+    def show_diff(self, dicts, fliter=False):
         for dict_ in dicts:
             for k, v in dict_.items():
+                if fliter == True:
+                    if v[0] == False and v[1] == True:
+                        continue
                 if v[0] != v[1]:
-                    print(k, v[0], v[1])
+                    print('\t', k, v[0], v[1])
 
     # 弃用
     # （辅助函数）打印两个数组不同处
@@ -139,30 +154,53 @@ def main():
             result.append(cal_result(reportme, report))
         return result
     
-    # 正式运算，耗时
-    def run_all():
+    # 正式运算，耗时，可设定最大计算个数 count、偏移量 shift
+    def run_all(count=100, shift=0, fliter=False, showdiff=False):
         results = []
+        index = 0
         ids = fileUtils.load_id_from_dir(JSON_DIR)
-        result = cal_report_to_others(450439, ids)
-        formated_result = resultUtils.format(result)
+        print('==================== 开始计算！====================')
+        print('总共需要对比的 report 数量：', len(ids))
         for report_id in ids:  # 遍历文件夹
+            index += 1
+            if index <= shift: continue
+            if index >= count + shift + 1: # 设定最多处理的条数，否则每次都跑完所有的数据会很耗时
+                return resultUtils.combine(results)
+            ## start
+            print('进度：%.3f%%' %(100 * (index - shift) / count), '\tcurrent stack_id: ' + str(report_id))
             result = cal_report_to_others(report_id, ids)
-            # resultUtils.show_diff(result) # 会打印运算过程中与真实结果不一致的 id，便于手动检验，非 debug 时可删去该过程
-            formated_result = resultUtils.format(result)
+            if showdiff:
+                resultUtils.show_diff(result, fliter=fliter) # 会打印运算过程中与真实结果不一致的 id，便于手动检验，非 debug 时可删去该过程
+            formated_result = resultUtils.format(result, fliter=fliter)
             results.append(formated_result)
         return resultUtils.combine(results)
 
     # 示例运算，计算其中的一个 id 与 其余所有 的结果
-    def run_demo(demo_id):
+    def run_demo(demo_id, fliter=False, showdiff=False):
         ids = fileUtils.load_id_from_dir(JSON_DIR)
         result = cal_report_to_others(demo_id, ids)
-        resultUtils.show_diff(result) # 会打印运算过程中与真实结果不一致的 id，便于手动检验，非 debug 时可删去该过程
-        formated_result = resultUtils.format(result)
+        if showdiff:
+            resultUtils.show_diff(result, fliter=fliter) # 会打印运算过程中与真实结果不一致的 id，便于手动检验，非 debug 时可删去该过程
+        formated_result = resultUtils.format(result, fliter=fliter)
         return formated_result
 
     # 启动代码：
-    # result = run_all()
-    # print(classification_report(result['real_list'], result['pred_list']))
-    result = run_demo(450439)
+    print('配置：')
+    start = 101
+    end = 110
+    fliter = False
+    showdiff = False
+    print('开始角标：', start)
+    print('结束角标：', end)
+    print('需要计算的 stack 数据量：', end - start)
+    print('是否过滤可能人工未标注的结果：', fliter)
+    print('是否打印预测不一致结果：', showdiff) # 即：预测结果与原始结果不一致的情况
+
+    result = run_all(count=(end - start), shift=start, fliter=fliter, showdiff=showdiff)
+    print('F1-Score 结果：')
     print(classification_report(result['real_list'], result['pred_list']))
+    # print('run: stack_id:: 450132')
+    # result = run_demo(450132)
+    # print(classification_report(result['real_list'], result['pred_list']))
+
 main()
--- dataset/f1.py
+++ dataset/f1.py
@@ -1,7 +1,11 @@
 from sklearn.metrics import classification_report
-
+import os
 # y_true = [0, 1, 2, 2, 0]
 # y_pred = [0, 1, 2, 2, 3]
-y_true = ['a', 1, 2, 2, 0]
+y_true = ['a', 1, 2, 2, 3]
 y_pred = ['a', 1, 2, 2, 3]
 print(classification_report(y_true, y_pred))
+
+
+filenames = os.listdir('json')
+print(len(filenames))
